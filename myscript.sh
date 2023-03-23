ddfb0672ab5396f1724b3035dba563ca2619b876
Alan Gibran <lushwacka@comunidad.unam.mx>
2023-03-22 18:44:29 -0600

    Variables in bash

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Hello, World!"
+myname="Alan"
+myage="40"
 
-echo "My current working directory is: "
-pwd
+echo "Hello, my name is $myname"
+echo "I'm $myage years old"
+
+#subshell example
+files=$(ls)
+echo $files
+
+now=$(date)
+echo $now
+
+echo "My user is $USER"
